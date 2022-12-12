e4dfdc6398d549bfbc51ca141ce6e49f46641e74
mrAnderson2159 <there.is.no.spoon.2159@gmail.com>
2022-12-12 18:04:07 +0100

    TheHeader

---
+++ landing_page_vue/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <the-header></the-header>
+  <the-body></the-body>
+  <the-footer></the-footer>
+</template>
+
+<script>
+import TheHeader from './components/layout/TheHeader.vue'
+import TheBody from './components/layout/TheBody.vue'
+import TheFooter from './components/layout/TheFooter.vue'
+
+export default {
+  components: {
+    TheHeader, 
+    TheBody, 
+    TheFooter
+  },
+  created() {},
+  data() {
+    return {};
+  },
+  props: {},
+  methods: {},
+};
+</script>
+
+<style scoped></style>
---
+++ landing_page_vue/src/components/layout/TheHeader.vue
@@ -0,0 +1,32 @@
+<template>
+<nav class="navbar navbar-dark bg-dark">
+  <div class="container-fluid">
+    <a href="#" class="navbar-brand h1 mb-0 px-4 main">Noleggio Auto</a>
+  </div>
+</nav>
+</template>
+
+<script>
+export default {
+  name: "Test",
+  created() {},
+  data() {
+    return {};
+  },
+  props: {},
+  methods: {},
+};
+</script>
+
+<style scoped>
+  @import url('https://fonts.googleapis.com/css2?family=Diplomata+SC&display=swap');
+
+  nav.navbar * {
+    font-size: 1.618rem
+  }
+
+  a.main {
+    font-family: 'Diplomata SC';
+
+  }
+</style>
