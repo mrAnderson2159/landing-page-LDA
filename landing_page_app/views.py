4c91c6e7c82841a340f9f2032f81187207159d5d
mrAnderson2159 <there.is.no.spoon.2159@gmail.com>
2023-01-27 02:11:48 +0100

    update security

--- landing_page_app/views.py
+++ landing_page_app/views.py
@@ -125,11 +125,11 @@ def botcatcher(request, url):
         green(f"{w} WHITELIST PASS")
     except ObjectDoesNotExist:
         forbidden = {
-            r'php', r'admin/', r'debug', r'^\.', r'actuator', r'api', r'console',
+            r'php', r'admin/', r'debug', r'^\.|_', r'actuator', r'api', r'console',
             r'owa', r'solr', r'wp-content', r'bedesk', r'metrics', r'test',
             r'v2', r'HNAP1', r'config', r'druid', r'hudson', r'mat', r'mgmt',
             r'portal', r'robots', r'\.xml', r'apple', r'Telerik', r'manager',
-            r'shell',r'chmod|jaws', r'aws|yml', r'undefined', 
+            r'shell',r'chmod|jaws', r'aws|yml', r'undefined', r'js', r'profiler'
 
         }
 
