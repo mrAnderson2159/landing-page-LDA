c8a5239c239173af22112e7a96d381cda4bc136c
mrAnderson2159 <there.is.no.spoon.2159@gmail.com>
2023-01-29 16:22:42 +0100

    models import shortcat

--- landing_page_app/admin.py
+++ landing_page_app/admin.py
@@ -5,13 +5,16 @@ import typing
 
 # Register your models here.
 TYPES = dir(typing)
+fields = [field for field in dir(models) if field[0].isupper() and field not in TYPES ]
 
-for field in dir(models):
-    if field[0].isupper() and field not in TYPES:
-        model = getattr(models, field)
-        try:
-            # print(model)
-            admin.site.register(model)
-            print(f'Registered "{field}" model to admin site')
-        except AlreadyRegistered:
-            continue
+for field in fields:
+    try:
+        admin.site.register(getattr(models, field))
+        print(f'Registered "{field}" model to admin site')
+    except AlreadyRegistered:
+        continue
+
+print("\nImport shortcats")
+for field in fields:
+    print(f"from landing_page_app.models import {field}")
+print()
